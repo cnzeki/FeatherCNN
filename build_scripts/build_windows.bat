d444571bb24ab2b57ccf8aa8959e048b8f97c2d3
tianzekang <tianzekang@ysten.com>
2018-10-30 22:02:02 +0800

    Generate Visual Studio solution files for code reading.

--- build_scripts/build_windows.bat
+++ build_scripts/build_windows.bat
@@ -1,10 +1,20 @@
-call ndk-build
+mkdir build-windows
+set pthread_zip=pthreads-w32-2-9-1-release.zip
+if exist %pthread_zip% (
+    echo Found %pthread_zip%
+) else (
+    echo download ...
+    tools\wget ftp://sourceware.org/pub/pthreads-win32/%pthread_zip%
+)
 
-mkdir feather
-mkdir feather\include
-mkdir feather\armeabi-v7a
-
-copy /y  src\*.h                 feather\include
-copy /y  libs\armeabi-v7a\*.so   feather\armeabi-v7a
-del  feather\include\feather_simple_generated.h
-cmd
+if not exist %pthread_zip% (
+    echo "pthread not found"
+    echo "You can download manually from ftp://sourceware.org/pub/pthreads-win32/%pthread_zip%"
+    exit
+)
+echo "extract ..."
+tools\unzip  -o -q %pthread_zip% -d pthreads
+:: vs solution
+cd build-windows
+cmake -G "Visual Studio 14 2015 Win64"  -DCMAKE_INSTALL_PREFIX=.. ..
+cd ..
